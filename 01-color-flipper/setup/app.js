a38a3c6c33dae0475efb98be4c7f5ace9489b8f7
Adaeze Ndupu <andupu4@gmail.com>
2021-07-17 01:00:16 +0100

    Updated Color Flipper

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,13 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+let body = document.querySelector("body");
+let button = document.querySelector("#btn");
+let txt = document.querySelector(".color");
+
+button.addEventListener("click", ()=>{
+
+    let colors = ["red", "blue", "yellow", "green", "orange", "teal", "brown", "violet", "peach", "pink", "white", "black"];
+
+      let color = Math.floor(Math.random()*colors.length);
+
+    txt.innerHTML = colors[color];
+    body.style.backgroundColor  = colors[color];
+})
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,18 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+let body = document.querySelector("body");
+let button = document.querySelector("#btn");
+let txt = document.querySelector(".color");
+
+button.addEventListener("click", ()=>{
+
+    let hex = [1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+    let color ="#";
+
+    for (let i=0; i<6; i++){
+        color+=hex[Math.floor(Math.random()*hex.length)];
+    }
+    let convert = color.toString();
+
+    txt.innerHTML = convert
+    body.style.backgroundColor  = convert;
+})
